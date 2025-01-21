7c6cf3cae5cc3b6c207bdff1cee87212047227de
aws-archi-patrick <aws.archi.patrick@gmail.com>
2025-01-20 21:44:36 -0500

    added logs

--- src/app/services/example.service.ts
+++ src/app/services/example.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
 
 export interface Example {
   id: number;
@@ -11,12 +13,13 @@ export interface Example {
   providedIn: 'root',
 })
 export class ExampleService {
-  private apiUrl = '/api/examples';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   getExamples(): Observable<Example[]> {
     console.log("passe par getExamples");
-    return this.http.get<Example[]>(this.apiUrl);
+    console.log("using this url" + this.apiUrl);
+    return this.http.get<Example[]>(`${this.apiUrl}/examples`);
   }
 }
